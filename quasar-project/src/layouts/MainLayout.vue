bb904a389245d784f28d3a9f3f56cb5b6d1d87c5
Leandro Juren Lucas <leandrojuren@gmail.com>
2022-12-20 20:49:05 -0300

    Fixing Keep-Alive feature in Vue3 language.

--- quasar-project/src/layouts/MainLayout.vue
+++ quasar-project/src/layouts/MainLayout.vue
@@ -65,9 +65,9 @@
     </q-drawer>
 
     <q-page-container>
-      <keep-alive>
-        <router-view />
-      </keep-alive>
+      <router-view v-slot="{ Component }">
+        <keep-alive> <component :is="Component" /> </keep-alive>
+      </router-view>
     </q-page-container>
   </q-layout>
 </template>
